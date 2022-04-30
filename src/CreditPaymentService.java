1461bc76277a80b6869e89b7ae277d913a79c2a1
lokky55 <lokky55@mail.ru>
2022-04-30 13:26:04 +0300

    2st commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,9 @@
 public class CreditPaymentService {
-    public double calculate(int summ, int years, float percent) {
+    public float calculate(int summ, int years, float percent) {
         int months = years * 12;
-        double monthPayment = summ * ((percent / (100 * 12)) / (1 - (Math.pow((1 + (percent / (100 * 12))), -(months - 1)))));
+        float intRate = percent / (100 * 12);
+        float fraction = (float) (intRate / (1 - (Math.pow(1 + intRate, -months))));
+        float monthPayment = summ * fraction;
         return monthPayment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         double monthPayment = service.calculate(1_000_000, 3, 9.99F);
-        System.out.println("Ежемесячный платеж: " + monthPayment);
+        System.out.print("Ваш ежемесячный платеж: ");
+        System.out.printf("%.2f", monthPayment);
+        System.out.print(" рублей");
     }
 }
